196b84494729ccadbf873b02b200e395946f005b
lariodiniz <lariodiniz@gmail.com>
2015-10-04 22:22:29 -0300

    heroku conf

--- OramaBank/wsgi.py
+++ OramaBank/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OranaBank.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OramaBank.settings")
 
 application = DjangoWhiteNoise(get_wsgi_application())
